02c2adbe573f23a868013db969a9be7326921d7f
Labib Hiba <57593172+lalabib@users.noreply.github.com>
2021-12-03 21:50:33 +0700

    Remove redudant function

--- app/src/main/java/com/lalabib/project/mtv/data/remote/ApiResponse.kt
+++ app/src/main/java/com/lalabib/project/mtv/data/remote/ApiResponse.kt
@@ -6,8 +6,6 @@ class ApiResponse<T> (val status: StatusResponse, val body: T, val message: Stri
     companion object {
         fun <T> success(body: T): ApiResponse<T> = ApiResponse(SUCCESS, body, null)
 
-        fun <T> empty(msg: String, body: T): ApiResponse<T> = ApiResponse(EMPTY, body, msg)
-
         fun <T> error(msg: String, body: T): ApiResponse<T> = ApiResponse(ERROR, body, msg)
     }
 }
