18ddedd3f83d953dfd65e13016635dd68f0b6d22
Labib Hiba <57593172+lalabib@users.noreply.github.com>
2021-12-07 23:15:12 +0700

    Fix code

--- app/src/main/java/com/lalabib/project/mtv/utils/AppExecutors.kt
+++ app/src/main/java/com/lalabib/project/mtv/utils/AppExecutors.kt
@@ -12,10 +12,6 @@ class AppExecutors @VisibleForTesting constructor(
     private val mainThread: Executor
 ){
 
-    companion object {
-        private const val THREAD_COUNT = 3
-    }
-
     constructor() : this(
         Executors.newSingleThreadExecutor(),
         Executors.newFixedThreadPool(THREAD_COUNT),
@@ -34,5 +30,9 @@ class AppExecutors @VisibleForTesting constructor(
         override fun execute(command: Runnable) {
             mainThreadHandler.post(command)
         }
+    }
+    
+    companion object {
+        private const val THREAD_COUNT = 3
     }
 }
