1e80d6b2b22eb048c0561779222fcf9b0d0ffaa2
lalabib <labibla21@gmail.com>
2022-04-17 21:44:05 +0700

    MTv: Add url and secret code

--- app/src/main/java/com/lalabib/project/mtv/service/ApiClient.kt
+++ app/src/main/java/com/lalabib/project/mtv/service/ApiClient.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val baseUrl = url-API
+    private const val baseUrl = "https://api.themoviedb.org/3/"
     val instance: ApiEndPoint by lazy {
         val interceptor = if(BuildConfig.DEBUG) {
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
